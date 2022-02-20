bcececbaed8d27d632a92e06ee8761015ec9175a
The Vinh <nguyenthevinh1997@gmail.com>
2022-02-20 21:44:12 +0700

    Fix src eslint

--- src/pages/BlogPage/BlogPage.component.jsx
+++ src/pages/BlogPage/BlogPage.component.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-// import Toast from "patterns/Toast";
 import { Loading, Finish } from "patterns/Loader";
 import { Toast } from "patterns/Toast";
 import { Pagination } from "components/Pagination/Pagination.component";
-import { connect } from "react-redux";
-
-// import { setUserInfo, setHospital } from "../../redux/user/user.actions";
 import BlogService from "services/blog.service";
-// import { danger } from "redux/alert/alert.actions";
-
 import "./BlogPage.styles.scss";
 import Blog from "components/Blog/Blog.component";
 import { Link } from "react-router-dom";
@@ -53,18 +47,20 @@ class BlogPage extends React.Component {
     BlogService.getBlogsService(this.state.searchFilter)
       .then((result) => {
         this.setState({ blogsData: result.data.data, pagy: result.data.pagy });
-        // Toast("success", "成功");
       })
       .catch((err) => {
-        Toast("danger", "エラーが発生しました。");
+        Toast("danger", err.response.data.error);
       })
       .finally(() => Finish());
   };
 
   onPageChange = (currentPage) => {
-    this.setState({ searchFilter: { ...this.state.searchFilter, page: currentPage } }, () => {
-      this.getBlogsData();
-    });
+    this.setState(
+      { searchFilter: { ...this.state.searchFilter, page: currentPage } },
+      () => {
+        this.getBlogsData();
+      }
+    );
   };
   onKeyUp = (event) => {
     if (event.charCode === 13) {
@@ -89,7 +85,11 @@ class BlogPage extends React.Component {
               />
             </div>
             <div className="form-group col-md-2">
-              <select name="sort_by" className="form-control" onChange={this.handleChangeFilter}>
+              <select
+                name="sort_by"
+                className="form-control"
+                onChange={this.handleChangeFilter}
+              >
                 <option value="created_at">Sort by</option>
                 <option value="id">id</option>
                 <option value="title">title</option>
@@ -99,18 +99,28 @@ class BlogPage extends React.Component {
               </select>
             </div>
             <div className="form-group col-md-2">
-              <select name="sort_direction" className="form-control" onChange={this.handleChangeFilter}>
+              <select
+                name="sort_direction"
+                className="form-control"
+                onChange={this.handleChangeFilter}
+              >
                 <option value="desc">Sort direction</option>
                 <option value="asc">asc</option>
                 <option value="desc">desc</option>
               </select>
             </div>
             <div className="form-group col-md-3">
-              <button onClick={() => this.onPageChange(1)} className="btn btn-primary">
+              <button
+                onClick={() => this.onPageChange(1)}
+                className="btn btn-primary"
+              >
                 Search
               </button>
               <Link to="/blog/create">
-                <button className="btn btn-primary" style={{ marginLeft: "10px" }}>
+                <button
+                  className="btn btn-primary"
+                  style={{ marginLeft: "10px" }}
+                >
                   Create
                 </button>
               </Link>
@@ -120,7 +130,14 @@ class BlogPage extends React.Component {
 
         {this.state.blogsData &&
           this.state.blogsData.map((item) => (
-            <Blog key={item.id} id={item.id} image={item.image} title={item.title} content={item.content} created_at={item.created_at}></Blog>
+            <Blog
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              content={item.content}
+              created_at={item.created_at}
+            ></Blog>
           ))}
         {this.state.pagy.pages !== 1 ? (
           <Pagination
@@ -136,7 +153,5 @@ class BlogPage extends React.Component {
     );
   }
 }
-const mapStateToProps = () => ({});
-const mapDispatchToProps = (dispatch) => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(BlogPage);
+export default BlogPage;
